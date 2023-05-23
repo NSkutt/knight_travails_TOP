d72cbb88cbe04e3c9d40a5364ddfd2c9708dba94
N Skutt <nskutt@protonmail.com>
2023-05-23 17:13:58 -0500

    add location tracking for pieces and a way to tell if a square is occupied

--- knight_travails.rb
+++ knight_travails.rb
@@ -4,9 +4,19 @@
 class Board
   attr_reader :coords
 
-  def initialize
+  def initialize(places)
     rankandfile = (1..8).to_a
     @coords = []
+    @locations = {}
     rankandfile.repeated_permutation(2) { |perm| board.push(perm) }
+    places.each { |piece| @locations[piece[0]] = piece[1] }
+  end
+
+  def piece_positions(piece, pos)
+    @locations[piece] = pos
+  end
+
+  def occupied?(coords)
+    @locations.key?(coords)
   end
 end
