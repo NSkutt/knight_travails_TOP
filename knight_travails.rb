07a813955c0f6ee3514a6a86ec73be4154afa2d8
N Skutt <nskutt@protonmail.com>
2023-05-23 14:27:14 -0500

    add board

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+# The game board to play on
+class Board
+  attr_reader :coords
+
+  def initialize
+    rankandfile = (1..8).to_a
+    @coords = []
+    rankandfile.repeated_permutation(2) { |perm| board.push(perm) }
+  end
+end
