6b2f6aed42719a4a7c20aeea193723279abd0493
N Skutt <nskutt@protonmail.com>
2023-05-25 18:31:28 -0500

    add the Node class and an initial (non-working) implentation of a new board creation method

--- knight_travails.rb
+++ knight_travails.rb
@@ -4,12 +4,17 @@
 class Board
   attr_reader :coords
 
-  def initialize(places)
-    rankandfile = (1..8).to_a
-    @coords = []
-    @locations = {}
-    rankandfile.repeated_permutation(2) { |perm| board.push(perm) }
-    places.each { |piece| @locations[piece[0]] = piece[1] }
+  def initialize
+    @grid = build_board
+    p @grid
+  end
+
+  def build_board(coords = [0, 0])
+    n = Node.new(coords)
+    n.files[0] = build_board([coords[0] + 1, coords]) while coords[0] < 8
+    n.files[1] = build_board([coords[0] - 1, coords]) while coords[0] > 1
+    n.ranks[0] = build_board([coords, coords[1] + 1]) while coords[1] < 8
+    n.ranks[1] = build_board([coords, coords[1] - 1]) while coords[1] > 1
   end
 
   def piece_positions(piece, pos)
@@ -20,3 +25,17 @@ class Board
     @locations.key?(coords)
   end
 end
+
+# This will form the 'squares' or nodes
+class Node
+  attr_reader :square
+  attr_writer :ranks, :files
+
+  def initialize(coords)
+    @square = coords
+    @ranks = [nil, nil]
+    @files = [nil, nil]
+  end
+end
+
+test = Board.new
